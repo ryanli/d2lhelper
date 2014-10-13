7b632fe07358f2efd46cce68736e38d6e9e1d411
Ryan Li <ryanli@ryanli.org>
2014-10-13 11:25:50 +0800

    item-value.js: update for new d2l ui.

--- item-value.js
+++ item-value.js
@@ -1,6 +1,6 @@
 $('.item:not(:has(.value))').each(function(i) {
   // don't use the .smallimg's alt attr since it doesn't have quality prefix
-  var itemName = $($($(this).children('.name')[0]).children('b')[0]).text().trim();
+  var itemName = $($($($(this).parent()).children('.name')[0]).children('b')[0]).text().trim();
   var url = 'http://steamcommunity.com/market/priceoverview/?currency=1&appid=570&market_hash_name=' + encodeURIComponent(itemName);
   $.ajax({
     url: url,
